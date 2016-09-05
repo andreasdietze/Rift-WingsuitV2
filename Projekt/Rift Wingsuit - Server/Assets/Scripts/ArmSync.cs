78dfaf6beb51820d7af9256e6ef69c54ac44537f
Andreas Dietze <stag_o_saurus@hotmail.com>
2016-09-05 05:20:30 +0200

    Update missing Ouculus assets

---
+++ Projekt/Rift Wingsuit - Server/Assets/Scripts/ArmSync.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmSync : MonoBehaviour
+{
+    // Script access
+    private NetworkManager nManager;
+    private Player player;
+    private HSDOutputText kinectOutput;
+
+    // Arm transformation
+    private float rot = 0.0f;
+    private Transform leftShoulder;
+    private Transform rightShoulder;
+
+    void Start()
+    {
+        // Find networkManager
+        nManager = (NetworkManager)GameObject.FindGameObjectWithTag("Network").GetComponent("NetworkManager");
+        // Funzt leider nicht -> Arme seperat aber nicht suit
+        leftShoulder = GameObject.Find("ShoulderL").transform;
+        Debug.Log(leftShoulder.position);
+        rightShoulder = GameObject.Find("ShoulderR").transform;
+
+        kinectOutput = (HSDOutputText)GameObject.FindGameObjectWithTag("MainCamera").GetComponent("HSDOutputText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot = kinectOutput.GetDeltaZ();
+        // Check if client has joined a server.
+        // This is necessary because the playerprefab is automatically generated.
+        if (nManager.serverInitiated) //serverJoined)
+        {
+            try
+            {
+                //player = (Player)GameObject.FindGameObjectWithTag ("Player").GetComponent("Player");
+                //armPosition = player.syncEndOVRRotation;//lerpedOVRRotation;
+                leftShoulder.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rot));
+                rightShoulder.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rot));
+            }
+            catch (UnityException e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+    }
+
+    bool showText = true;
+    Rect textArea = new Rect(300, 60, Screen.width, Screen.height);
+
+    private void OnGUI()
+    {
+        //if(nManager.serverJoined && showText)
+        //GUI.Label(textArea, headOrientation.ToString());
+    }
+
+}
