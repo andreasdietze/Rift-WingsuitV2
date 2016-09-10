e001011c79f9816585757f8533f0f6742437df6c
Andreas Dietze <stag_o_saurus@hotmail.com>
2016-09-10 16:13:02 +0200

    Level extension: - Ringsetup - Water - Trees - Bridge - World refactoring

---
+++ Projekt/Rift Wingsuit - Server/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+	Rect crosshairRect;
+	public Texture crosshairTexture;
+	public float crosshairSize;
+
+	// Use this for initialization
+	void Start () {
+		//crosshairSize = Screen.width * 0.1f;
+		//crosshairTexture = Resources.Load ("Images/crosshair") as Texture;
+		crosshairRect = new Rect (Screen.width / 2 - crosshairSize / 2, Screen.height / 2 - crosshairSize / 2, crosshairSize, crosshairSize);
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+	
+	}*/
+
+	void OnGUI()
+	{
+		GUI.DrawTexture (crosshairRect, crosshairTexture);
+	}
+}
---
+++ Projekt/Rift Wingsuit - Server/Assets/Scripts/RadialProgressBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//https://www.youtube.com/watch?v=f6dEj7-G-Fw
+
+public class RadialProgressBar /*: MonoBehaviour*/ {
+
+	public Transform LoadingBar;
+	[SerializeField] public float currentAmount;
+	[SerializeField] public float speed;
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}*/
+	
+	// Update is called once per frame
+	/*void Update () {
+
+	}*/
+
+	void Progress (){
+		if (currentAmount < 100) {
+			currentAmount += speed * Time.deltaTime;
+			
+		} else {
+			
+		}
+		LoadingBar.GetComponent<Image> ().fillAmount = currentAmount / 100;
+	}
+}
